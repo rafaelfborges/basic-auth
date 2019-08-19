7324a8967de3d355c4a9b5b6e8f72d9335977675
Rafael Borges <rafaelfborges@gmail.com>
2019-08-19 03:51:37 -0300

    Subindo segunda parte do exercício

--- SistemaAutenticacao/src/br/com/rafaelfborges/auth/Principal.java
+++ SistemaAutenticacao/src/br/com/rafaelfborges/auth/Principal.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 /**
  * @author Rafael Fernando Borges
- * Execício disciplina Segurança da Informação.
+ * Execício p/ disciplina Segurança da Informação.
  */
 public class Principal {
 
@@ -19,11 +19,9 @@ public class Principal {
 		Sistema sistema = new Sistema();
 		Usuario usuario1 = new Usuario("rafael", "12345");
 		Usuario usuario2 = new Usuario("fernando", "54321");
-		Usuario usuario3 = new Usuario("borges", "09876");
 		
 		sistema.cadastrarUsuario(usuario1);
 		sistema.cadastrarUsuario(usuario2);
-		sistema.cadastrarUsuario(usuario3);
 		
 		sc = new Scanner(System.in);
 		
